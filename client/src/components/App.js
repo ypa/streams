4cd9c9cc7ac1f09775cf7a33343f22ba68e2c689
Yan P Aung <yan.pye.aung@gmail.com>
2020-09-29 07:14:37 -0700

    Scaffold stream CRUD components and wire them up to Routes with react-router-dom BrowserRouter

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-
-const PageOne = () => {
-  return <div>PageOne</div>;
-}
-
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <button>Click Me!</button>
-    </div>
-  );
-}
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 
 const App = () => {
@@ -20,8 +12,11 @@ const App = () => {
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
