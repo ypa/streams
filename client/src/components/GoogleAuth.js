346969f6bf77e528df557931a52f8c0fd3b402e2
Yan P Aung <yan.pye.aung@gmail.com>
2020-10-01 07:25:30 -0700

    Initialize Google client api for OAuth2 in GoogleAuth's componentDidMount life cycle method as we only want to initialize once when the component is loaded

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: '306011753765-4h5o1utls7dlbf1dt22lq5u9lo5oh7m7.apps.googleusercontent.com',
+        scope: 'email'
+      });
+    });
+  }
+
   render () {
     return <div>GoogleAuth</div>
   }
