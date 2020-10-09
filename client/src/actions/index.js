0c741118127412bd92c174d780a31611a82ae0e7
Yan P Aung <yan.pye.aung@gmail.com>
2020-10-09 07:00:20 -0700

    Wire up axios, redux-thunk and react-redux connect for creating streams on the backend apis

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,5 @@
+import streams from '../apis/streams';
 import { SIGN_IN, SIGN_OUT } from './types';
-
-
 
 export const signIn = (userId) => {
   return {
@@ -9,9 +8,12 @@ export const signIn = (userId) => {
   };
 };
 
-
 export const signOut = () => {
   return {
     type: SIGN_OUT
   };
+};
+
+export const createStream = (formValues) => async (dispatch) => {
+  streams.post('/streams', formValues);
 };
--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -24,8 +26,8 @@ class StreamCreate extends React.Component {
     );
   }
 
-  onSubmit(formValues) {
-    console.log(formValues);
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
   }
 
   render() {
@@ -53,7 +55,9 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate: validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
