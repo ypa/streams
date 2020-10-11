8c95e9417a48a7f6d3c854ab600aa382671f2911
Yan P Aung <yan.pye.aung@gmail.com>
2020-10-11 08:12:01 -0700

    Load up StreamList component with fetchStreams action creator when componentDidMount and map over state to props and render JSX

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,8 +1,45 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
-  return <div>StreamList</div>
-};
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
 
-export default StreamList;
+  renderList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">
+          {this.renderList()}
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  // Pull out as array from object for easier to map over
+  // when rendering inside the component.
+  // We had put state as object with ids as keys so that
+  // it's easier for reducers to create new state object.
+  return { streams: Object.values(state.streams) };
+}
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
 
