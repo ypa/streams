5f90427b21254038b61ab7b18e7abb0d6dda8a21
Yan P Aung <yan.pye.aung@gmail.com>
2020-10-18 07:38:56 -0700

    Make the modal reusable by configuring it from props and have DeleteStream component pass down those props

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -6,13 +6,12 @@ const Modal = props => {
   return ReactDOM.createPortal(
     <div onClick={() => history.push('/')} className="ui dimmer modals visible active">
       <div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
-        <div className="header">Delete Stream</div>
+        <div className="header">{props.title}</div>
         <div className="content">
-          Are you sure you want to delete this stream?
+          {props.content}
         </div>
         <div className="actions">
-          <div class="ui negative button">Delete</div>
-          <div className="ui cancel button">Cancel</div>
+          {props.actions}
         </div>
       </div>
     </div>,
